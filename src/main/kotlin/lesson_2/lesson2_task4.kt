4ad58d6cc67614d4d930a948fff459ecacb29da9
Михаил Курочкин <eaq19933@gmail.com>
2024-06-07 13:02:39 +0300

    KS-2-4 crtd buffed inform

---
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_2
+
+fun main() {
+
+    val POWER_BAFF_PERCENT = 20
+    val DEFAULT_PERCENT = 100
+    val minedCrystalOre = 7f
+    val minedIronOre = 11f
+    val buffedCrystalOre = minedCrystalOre / DEFAULT_PERCENT * POWER_BAFF_PERCENT
+    val buffedIronOre = minedIronOre / DEFAULT_PERCENT * POWER_BAFF_PERCENT
+
+    println("Бонусная кристалическая руда: ${buffedCrystalOre.toInt()}.\nБонусная железная руда: ${buffedIronOre.toInt()}")
+
+}
