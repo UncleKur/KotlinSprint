723a9d730ad7b18b993c20402c5c97d230aea2b5
Михаил Курочкин <eaq19933@gmail.com>
2024-06-08 11:53:40 +0300

    KS-2-4 rfct output on screen

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -7,11 +7,13 @@ fun main() {
     val surName: String = "Сергеевна"
     var age: Int = 20
 
+    print("ФИО и возраст на момент $age лет: ")
     println("$lastName $firstName $surName, $age")
 
     lastName = "Сидорова"
     age = 22
 
+    print("ФИО и возраст на момент $age лет: ")
     println("$lastName $firstName $surName, $age")
 
 }
