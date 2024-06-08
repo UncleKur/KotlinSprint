f148a8fa24841a4315fa9dc5f151bd5a0984e137
Михаил Курочкин <eaq19933@gmail.com>
2024-06-09 02:47:16 +0300

    KS-3-5 rfctr code

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -3,32 +3,16 @@ package org.example.lesson_3
 fun main() {
 
     val getMoveInformation = "D2-D4;0"
-    val getFromField: String = getMoveInformation.substringBefore('-')
-    val getToField: String = getMoveInformation.substringAfter('-').substringBefore(';')
-    val getMoveNumber: Int = getMoveInformation.substringAfter(';').toInt()
+    val getServerParse = getMoveInformation.split('-', ';')
+    val getFromField: String = getServerParse[0]
+    val getToField: String = getServerParse[1]
+    val getMoveNumber: Int = getServerParse[2].toInt()
     val infoFromServer: String = """
         Сервер получил информацию от игрока:
         Откуда: $getFromField
         Куда: $getToField
         Номер хода: $getMoveNumber
     """.trimIndent()
-    var moveNumber: Int = 0
-    var fromField: String = "E2"
-    var toField: String = "E4"
-    var infoAbout: String = "$fromField-$toField;${++moveNumber}"
-    println(infoAbout)
-
-    fromField = "E7"
-    toField = "E5"
-    moveNumber++
-    infoAbout = "$fromField-$toField;$moveNumber"
-    println(infoAbout)
-
-    fromField = "D2"
-    toField = "D3"
-    moveNumber++
-    infoAbout = "$fromField-$toField;$moveNumber"
-    println(infoAbout)
 
     println(infoFromServer)
 
