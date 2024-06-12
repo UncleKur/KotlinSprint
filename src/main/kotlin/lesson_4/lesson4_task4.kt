1046e7b6d3ff7b8c9afb9355d351eeb93328d60e
Михаил Курочкин <eaq19933@gmail.com>
2024-06-12 12:48:42 +0300

    KS-4-4 rfctr added isOdd val

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,16 +1,13 @@
 package org.example.lesson_4
 
-/**
- * Если я верно понял условия задачи. Чел тренит 7 дней в неделю.
- * И если тренировки рук/ног начинались с нечётного дня, соотвествтенно эту проверку я и выполняю
- */
 fun main() {
 
     val dayOfTraining = 5
-    val isHandsTraining = dayOfTraining % 2 != 0
-    val isLegsTraining = dayOfTraining % 2 == 0
-    val isBackTraining = dayOfTraining % 2 == 0
-    val isPressTraining = dayOfTraining % 2 != 0
+    val isOdd = dayOfTraining % 2 != 0
+    val isHandsTraining = isOdd
+    val isLegsTraining = !isOdd
+    val isBackTraining = !isOdd
+    val isPressTraining = isOdd
     val trainingInformation = """
         Сегодня $dayOfTraining день тренировки и нужно выполнить:
         Упражнения для рук: $isHandsTraining
