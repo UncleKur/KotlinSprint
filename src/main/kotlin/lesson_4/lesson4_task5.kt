7eb763f6e9faf8ac9a6293f806fc992b9adf83b3
Михаил Курочкин <eaq19933@gmail.com>
2024-06-12 13:01:45 +0300

    KS-4-4 rfctr using readln() method and fix code formating

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -3,32 +3,30 @@ package org.example.lesson_4
 fun main() {
 
     print("У коробля отсутствуют повреждения?\nВведите (true - да, false - нет): ")
-    val isHealthShip = readlnOrNull()?.toBoolean() ?: return
+    val isHealthShip = readln().toBoolean()
 
     print("Введите количество членов экипажа: ")
-    val membershipAmount = readlnOrNull()?.toIntOrNull() ?: return
+    val membershipAmount = readln().toInt()
 
     print("Введите количество ящиков провизии: ")
-    val suppliesAmount = readlnOrNull()?.toIntOrNull() ?: return
+    val suppliesAmount = readln().toInt()
 
     print("Благоприятная ли погода?\nВведите (true - да, false - нет): ")
-    val isGoodWeather = readlnOrNull()?.toBoolean() ?: return
+    val isGoodWeather = readln().toBoolean()
 
-    val canHealthyShipStartJourney =
-        (isHealthShip == SHIP_FULL_HEALTH)
-                && (membershipAmount in MIN_MEMBERSHIP..MAX_MEMBERSHIP)
-                && (suppliesAmount > MINIMAL_SUPPLIES)
+
+    val canHealthyShipStartJourney = isHealthShip == SHIP_FULL_HEALTH
+                && membershipAmount in MIN_MEMBERSHIP..MAX_MEMBERSHIP
+                && suppliesAmount > MINIMAL_SUPPLIES
                 && ((isGoodWeather == GOOD_WEATHER) || (isGoodWeather != GOOD_WEATHER))
 
-    val canDamagedShipStartJourney =
-        (isHealthShip != SHIP_FULL_HEALTH)
-                && (membershipAmount == MAX_MEMBERSHIP)
-                && (suppliesAmount >= MINIMAL_SUPPLIES)
-                && (isGoodWeather == GOOD_WEATHER)
+    val canDamagedShipStartJourney = isHealthShip != SHIP_FULL_HEALTH
+                && membershipAmount == MAX_MEMBERSHIP
+                && suppliesAmount >= MINIMAL_SUPPLIES
+                && isGoodWeather == GOOD_WEATHER
 
     println(
-        if (isHealthShip)
-            "Корабль без повреждений может отправиться в плавание? $canHealthyShipStartJourney"
+        if (isHealthShip) "Корабль без повреждений может отправиться в плавание? $canHealthyShipStartJourney"
         else "Корабль с повреждениями может отправиться в плавание? $canDamagedShipStartJourney"
     )
 }
