e529dd131cb99456533bfddeedb9b3ec7a759ac8
Михаил Курочкин <eaq19933@gmail.com>
2024-06-13 16:08:29 +0300

    KS-5-4 rmv IfElse fun and rename When func

--- src/main/kotlin/lesson_5/lesson5_task4.kt
+++ src/main/kotlin/lesson_5/lesson5_task4.kt
@@ -2,11 +2,10 @@ package org.example.lesson_5
 
 fun main() {
 
-    taskSolutionWithWhen()
-    taskSolutionWithIfElse()
+    checkUserIdentity()
 }
 
-fun taskSolutionWithWhen() {
+fun checkUserIdentity() {
     println("Чтобы войти в систему, индетифицируйте себя!")
     print("Введите имя пользователя: ")
     val accessToShipResult = when (val userLoginInput = readln()) {
@@ -19,26 +18,10 @@ fun taskSolutionWithWhen() {
                 "Неверный пароль!"
             }
         }
+
         else -> "Такого пользователя не существует. Пожалуйста, зарегистрируйтесь!"
     }
     println(accessToShipResult)
-}
-fun taskSolutionWithIfElse() {
-    println("Чтобы войти в систему, индетифицируйте себя!")
-    print("Введите имя пользователя: ")
-    val userLoginInput = readln()
-    val isLoginRegistered = userLoginInput == USER_LOGIN
-    if (!isLoginRegistered) {
-        println("Такого пользователя не существует. Пожалуйста, зарегистрируйтесь!")
-    } else {
-        print("Введите Ваш пароль: ")
-        val userPasswordInput = readln()
-        if (userPasswordInput == USER_PASSWORD) {
-            println("Добро пожаловать на борт, $userLoginInput!")
-        } else {
-            println("Неверный пароль!")
-        }
-    }
 }
 
 const val USER_LOGIN = "Zaphod"
